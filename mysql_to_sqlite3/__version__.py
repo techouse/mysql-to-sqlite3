2e15d0ac01efae5e2be956e490bb214c54b0943e
Klemen Tušar <techouse@gmail.com>
2021-02-12 22:17:27 +0000

    :bookmark: Version bump

--- mysql_to_sqlite3/__version__.py
+++ mysql_to_sqlite3/__version__.py
@@ -2,7 +2,7 @@
 __title__ = "mysql-to-sqlite3"
 __description__ = "A simple Python tool to transfer data from MySQL to SQLite 3"
 __url__ = "https://github.com/techouse/mysql-to-sqlite3"
-__version__ = "1.3.6"
+__version__ = "1.3.7"
 __author__ = "Klemen Tusar"
 __author_email__ = "techouse@gmail.com"
 __license__ = "MIT"
