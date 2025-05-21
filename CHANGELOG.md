cdee3dad0c8f725820c3fd291397ad5da059e21e
Klemen Tušar <techouse@gmail.com>
2025-05-21 23:50:41 +0100

    :bookmark: release v2.4.1
    # 2.4.1
    
    * [FIX] fix passwordless login

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 2.4.1
+
+* [FIX] fix passwordless login
+
 # 2.4.0
 
 * [CHORE] drop support for Python 3.8
--- src/mysql_to_sqlite3/__init__.py
+++ src/mysql_to_sqlite3/__init__.py
@@ -1,5 +1,5 @@
 """Utility to transfer data from MySQL to SQLite 3."""
 
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 
 from .transporter import MySQLtoSQLite
